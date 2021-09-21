022a86428a6864598768a447997d187955e07223
Pankaj <442001ps@gmail.com>
2021-09-21 16:28:46 +0530

    initial

---
+++ Breakball/src/breakball/Main.java
@@ -0,0 +1,13 @@
+package breakball;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame F1 = new JFrame("Breakball");
+        F1.setSize(1366, 768);
+        F1.setVisible(true);
+        F1.setLayout(null);
+    }
+}
